1abb97d5a1003e1a801954a6d5f99deef7af1dae
devilforlife123 <surajpoudelg@gmail.com>
2020-02-24 09:49:49 +1100

    Initial Commit

---
+++ newsHeadlineQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsHeadlineQuiz
+//
+//  Created by suraj poudel on 24/2/20.
+//  Copyright © 2020 suraj poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
